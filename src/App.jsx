54051d0fc105653f33c3d99217dcd9badc6bb59b
Shubhi Jain <shubhij2511@gmail.com>
2024-11-11 23:21:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+function App() {
+  const [color, setcolor] = useState("olive")
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200"
+        style = {{backgroundColor: color}}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className='flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-xl'>
+            <button 
+            onClick={() => setcolor("red")}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style={{backgroundColor: "red" }}>
+              Red</button>
+            <button 
+            onClick={() => setcolor("green")}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style={{backgroundColor: "green" }}>
+              Green</button>
+            <button
+            onClick={() => setcolor("blue")} 
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style={{backgroundColor: "blue" }}>
+              Blue</button>
+            <button 
+            onClick={() => setcolor("pink")}className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style={{backgroundColor: "pink" }}>
+              Pink</button>
+            <button
+            onClick={() => setcolor("aqua")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style={{backgroundColor: "aqua" }}>
+              Aqua</button>
+            <button 
+            onClick={() => setcolor("purple")}className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+            style={{backgroundColor: "purple" }}>
+              Purple</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
